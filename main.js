365f29197b00a22565c5ecf18b6133eee1a22c8a
Exkave <77685466+ExKave@users.noreply.github.com>
2024-08-18 12:53:24 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
